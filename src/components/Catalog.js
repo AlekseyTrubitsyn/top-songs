991a4523498a68e044a5e66676e9771bf42a48e8
Aleksey Trubtisyn <at.8441451@yandex.ru>
2018-02-26 13:27:00 +0300

    create add button and empty catalog background

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -3,24 +3,34 @@ import PropTypes from "prop-types";
 
 import CatalogItem from "./CatalogItem";
 
-const Catalog = ({items, onSelect}) => (
-  <div className="catalog">
-    {items.map(item => (
-      <CatalogItem
-        key={item.id}
-        songName={item.name}
-        artistName={item.artistName}
-        imageSource={item.artworkUrl100}
-        artistUrl={item.artistUrl}
-        songUrl={item.url}
-        genre={item.genres[0].name}
-        releaseDate={item.releaseDate}
-        isFavorite={item.isFavorite}
-        onSelect={(e) => onSelect(e, item.id, item.isFavorite)}
-      />
-    ))}
-  </div>
-)
+const Catalog = ({items, onSelect}) => {
+  if (items.length === 0) {
+    return (
+      <div className="catalog is-empty">
+        <p>Здесь ничего нет...</p>
+      </div>
+    )
+  }
+
+  return (
+    <div className="catalog">
+      {items.map(item => (
+        <CatalogItem
+          key={item.id}
+          songName={item.name}
+          artistName={item.artistName}
+          imageSource={item.artworkUrl100}
+          artistUrl={item.artistUrl}
+          songUrl={item.url}
+          genre={item.genres[0].name}
+          releaseDate={item.releaseDate}
+          isFavorite={item.isFavorite}
+          onSelect={(e) => onSelect(e, item.id, item.isFavorite)}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default Catalog;
 
--- src/components/CatalogItem.js
+++ src/components/CatalogItem.js
@@ -6,15 +6,21 @@ const CatalogItem = ({ songName, artistName, imageSource, artistUrl, songUrl, ge
   className = (isFavorite) ? className + " " + className + "--selected" : className;
   songName = (songName.length > 22) ? songName.slice(0, 20) + "..." : songName;
   artistName = (artistName.length > 22) ? artistName.slice(0, 20) + "..." : artistName;
+  let likeButtonClassName = (isFavorite) ? "catalog-item__like-button catalog-item__like-button--delete" : "catalog-item__like-button";
 
   return (
-    <div className={className} onClick={onSelect}>
+    <div className={className}>
       <img
         className="catalog-item__image"
         src={imageSource}
         width="200"
         height="200"
         alt={songName} />
+      <button
+        className={likeButtonClassName}
+        onClick={onSelect}>
+        {/* <span className="visually-hidden">{isFavorite ? "dislike" : "like"}</span> */}
+      </button>
       <p className="catalog-item__title">
         <a className="catalog-item__link" href={songUrl}>{songName}</a>
       </p>
